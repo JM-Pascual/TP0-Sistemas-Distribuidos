4c1831b35ebd69fbaf6459b8026241bfafe77dca
JM-Pascual <jmpascualosorio@gmail.com>
2025-03-20 11:48:44 -0300

    feat: changed image to Alpine for faster testing

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# Docker run utilizando una imágen de Ubuntu y ejecutando 3 comandos
+# Docker run utilizando una imágen de Alpine y ejecutando 3 comandos
 # Actualizacion del manejador de paquetes apt e instalacion de netcat
 # Validacion de la respuesta del servidor echo
 # echo del resultado de la validacion
-docker run --rm -it --network=tp0_testing_net ubuntu:latest bash -c \
-    "apt update && apt-get install -y netcat-openbsd && \
-    if response=\$(echo 'Hello!' | nc server 12345) && [ \"\$response\" == \"Hello!\" ]; then \
-        echo 'action: test_echo_server | result: success'; \
-    else \
-        echo 'action: test_echo_server | result: fail'; \
-    fi"
+docker run -d --rm --network=tp0_testing_net --name test_echo_server alpine:latest sh -c "while true; do sleep 1; done"
+
+docker exec test_echo_server apk update
+
+docker exec test_echo_server apk add netcat-openbsd
+
+response=$(echo 'Hello!' | docker exec -i test_echo_server nc server 12345)
+if [ "$response" == "Hello!" ]; then
+    echo 'action: test_echo_server | result: success'
+else
+    echo 'action: test_echo_server | result: fail'
+fi
+
+docker stop test_echo_server
