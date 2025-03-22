e10cbc7c25505176b13653217a0ca48f569437f1
JM-Pascual <jmpascualosorio@gmail.com>
2025-03-22 17:58:00 -0300

    feat: added validation in host machine and simplification of nc command call

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -4,10 +4,20 @@
 # Actualizacion del manejador de paquetes apk e instalacion de netcat
 # Validacion de la respuesta del servidor echo
 # echo del resultado de la validacion
-docker run --rm -it --network=tp0_testing_net alpine:latest sh -c \
-    "apk update && apk add --no-cache netcat-openbsd && \
-    if response=\$(echo 'Hello!' | nc -w 10 server 12345) && [ \"\$response\" == \"Hello!\" ]; then \
-        echo 'action: test_echo_server | result: success'; \
-    else \
-        echo 'action: test_echo_server | result: fail'; \
-    fi"
+
+#docker run --rm -it --network=tp0_testing_net alpine:latest sh -c \
+#    "apk update && apk add --no-cache netcat-openbsd && \
+#    if response=\$(echo 'Hello!' | nc -w 10 server 12345) && [ \"\$response\" == \"Hello!\" ]; then \
+#        echo 'action: test_echo_server | result: success'; \
+#    else \
+#        echo 'action: test_echo_server | result: fail'; \
+#    fi"
+
+TESTING_MESSAGE="Hello"
+
+response=$(docker run -it --network=tp0_testing_net --rm alpine:latest sh -c "echo '$TESTING_MESSAGE' | nc -w 10 server 12345" | tr -d '\r\n')
+if [ "$response" = "$TESTING_MESSAGE" ]; then
+    echo "action: test_echo_server | result: success"
+else
+    echo "action: test_echo_server | result: fail"
+fi
