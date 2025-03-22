20f37b6b28fe3fa896e9a5f198633ca5117bec3f
JM-Pascual <jmpascualosorio@gmail.com>
2025-03-22 12:59:06 -0300

    feat: server graceful shutdown

--- server/common/server.py
+++ server/common/server.py
@@ -1,3 +1,4 @@
+import signal
 import socket
 import logging
 
@@ -8,6 +9,13 @@ class Server:
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
+        self._server_working = False
+
+    def graceful_shutdown(self):
+        # Cierre del socket del servidor
+        self._server_socket.close()
+        # Loggeo de la acción
+        logging.info('action: graceful_shutdown | result: success')
 
     def run(self):
         """
@@ -20,9 +28,21 @@ class Server:
 
         # TODO: Modify this program to handle signal to graceful shutdown
         # the server
-        while True:
-            client_sock = self.__accept_new_connection()
-            self.__handle_client_connection(client_sock)
+
+        # Seteo del handler para la señal de SIGTERM
+        signal.signal(signal.SIGTERM, self.graceful_shutdown)
+
+        # Seteo del handler para la señal de SIGINT
+        signal.signal(signal.SIGINT, self.graceful_shutdown)
+
+        self._server_working = True
+
+        try:
+            while self._server_working:
+                client_sock = self.__accept_new_connection()
+                self.__handle_client_connection(client_sock)
+        except OSError as e:
+            logging.error(f"action: accepting new connections | result: fail | error: {e}")
 
     def __handle_client_connection(self, client_sock):
         """
@@ -39,7 +59,7 @@ class Server:
             # TODO: Modify the send to avoid short-writes
             client_sock.send("{}\n".format(msg).encode('utf-8'))
         except OSError as e:
-            logging.error("action: receive_message | result: fail | error: {e}")
+            logging.error(f"action: receive_message | result: fail | error: {e}")
         finally:
             client_sock.close()
 
